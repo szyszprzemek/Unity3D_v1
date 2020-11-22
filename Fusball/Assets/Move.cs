b3685301fd68e18e2ab73e22a8dae84e0a8352aa
szyszprzemek <przemekszyszkiewicz@gmail.com>
2020-11-22 20:59:04 +0100

    Started writing new game Fusball

---
+++ Fusball/Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")){
+        	transform.Rotate(2f, 0f, 0f);
+        }
+
+        if (Input.GetKey("e")){
+        	transform.Rotate(-2f, 0f, 0f);
+        }
+    }
+}
